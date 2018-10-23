3452981c05528e2e9c2dcbe6f0bec8d490887ecd
mochaliao <peiju.patty@gmail.com>
2018-10-24 00:40:28 +0800

    pro-add

--- social/iami/application/language/english/picture_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-//新增圖片成功
-$lang['picture_add_success'] = 'Add picture success !!';
-//新增圖片失敗
-$lang['picture_add_failed'] = 'Add picture failed, please try again later !!';
-//圖片不存在
-$lang['picture_not_found'] = 'Picture not exists !!';
-//圖片識別碼未指定
-$lang['picture_no_picture_id'] = 'No picture identification !!';
